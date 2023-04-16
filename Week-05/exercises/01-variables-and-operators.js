d779a320dafaec6d5dc4aff6e7019350f8f1786e
MrJogt <42785472+MrJogt@users.noreply.github.com>
2023-04-16 18:24:25 -0300

    BaSP-W05-2023: exercise 01 completed

---
+++ Week-05/exercises/01-variables-and-operators.js
@@ -0,0 +1,33 @@
+console.log('--EJERCICIO 1: VARIABLES Y OPERADORES');
+
+/* a. Crear dos variables numéricas y utilizar el operador suma para 
+guardar el valor de la suma de ambos números en una 3er variable*/
+
+console.log('-Ejercicio 1.a:');
+
+var num1 = 2;
+var num2 = 6;
+var result = num1 + num2;
+
+console.log(result);
+
+/* b. Crear dos variables de tipo String y concatenarlas guardando el resultado en una 3er variable.*/
+
+console.log('-Ejercicio 1.b:')
+
+var first = 'He';
+var second = 'lado';
+var stringResult = first + second;
+
+console.log(stringResult);
+
+/*Crear dos variables de tipo String y sumar el largo de cada variable 
+(cantidad de letras del string) guardando el resultado de la suma en una 3er variable (utilizar length)*/
+
+console.log('-Ejercicio 1.c:');
+
+var word1 = 'perro';
+var word2 = 'gato';
+var wordLength = word1.length + word2.length;
+
+console.log(wordLength);
