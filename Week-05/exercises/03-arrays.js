2890224709468cf3b554750637fb7ff681cb758e
MrJogt <42785472+MrJogt@users.noreply.github.com>
2023-04-16 20:43:38 -0300

    BaSP-W05-2023: exercise 04 completed

--- Week-05/exercises/03-arrays.js
+++ Week-05/exercises/03-arrays.js
@@ -1,4 +1,4 @@
-console.log('--EJERCICIO 3 - ARRAYS');
+console.log('--EJERCICIO 3: ARRAYS');
 
 /* a. Dado el siguiente array: ["Enero", "Febrero", "Marzo", "Abril", "Mayo", "Junio", "Julio", "Agosto", 
 "Septiembre", "Octubre", "Noviembre", "Diciembre"] mostrar por consola los meses 5 y 11 (utilizar console.log).*/
