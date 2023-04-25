7d74fa8cb932b521ac8781882569dcd864f49e80
MrJogt <42785472+MrJogt@users.noreply.github.com>
2023-04-25 12:13:55 -0300

    BaSP-W06-2023: login.js code corrections and sign-up validations started

--- Week-06/js/login.js
+++ Week-06/js/login.js
@@ -1,17 +1,3 @@
-/*
-Se debe crear un archivo llamado login.js en donde se debe poner toda la lógica
-Javascript necesaria para el funcionamiento del formulario login.
-La validación de cada campo se debe realizar en el evento “blur” de cada uno de los campos.
-Además, si algún campo tiene un error de validación,
-en el evento “focus” de dicho campo debe desaparecer el mensaje
-porque se asume que el usuario está corrigiendo el error.
-
-Se debe agregar un botón “Login” que al presionarlo se muestre un alert
-con la información cargada en el formulario en caso de que haya pasado
-todas las validaciones. Si alguna validación no pasó,
-además de mostrar el error debajo del campo, también se debe mostrar el error en el alert.
-*/
-
 var emailExpression = /^[^@]+@[^@]+\.[a-zA-Z]{2,}$/;
 var emailText = document.getElementById("email-input");
 var passwordValue = document.getElementById("password-input");
@@ -45,7 +31,6 @@ function passwordBlur() {
   if (password.length > 8) {
     for (var i = 0; i < password.length; i++) {
       let char = password[i];
-
       if (char >= "A" && char <= "Z") {
         hasUpperCase = true;
       } else if (char >= "a" && char <= "z") {
---
+++ Week-06/js/sign-up.js
@@ -0,0 +1,4 @@
+var nameInp = document.getElementById("name-input");
+var lastNamInp = document.getElementById("last-name-input");
+var dniInp = document.getElementById("dni-input");
+var emailInp = document.getElementById("email-input");
